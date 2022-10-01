77de6dfb4c957493fda1db4123a36957d1196112
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-30 23:36:49 -0400

    adding TopBar component with Bootstrap packages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import TopBar from './components/TopBar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <TopBar/>
     </div>
   );
 }
