0e8eef1dc908c0ee610e7981a121e4aa1393ec04
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-30 23:56:40 -0400

    added background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
-import TopBar from './components/TopBar';
+import './App.css'
+import TopBar from './components/TopBar'
+import Background from './components/Background';
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
+      <Background/>
     </div>
   );
 }
