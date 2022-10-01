9e2687d875842f8e240982a8ba398b39ed15e70c
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-10-01 00:17:42 -0400

    fixed store listing text alignment

--- src/components/StoreListings.js
+++ src/components/StoreListings.js
@@ -18,6 +18,7 @@ function StoreListings() {
                             <p>Delivery * Pickup</p>
                         </Col>
                     </Row>
+
                     <Col sm="1"></Col>
                     <Row >
                         <Col xs=".5">
@@ -28,12 +29,13 @@ function StoreListings() {
                             <p>Delivery * Pickup</p>
                         </Col>
                     </Row>
+
                     <Col sm="1"></Col>
                     <Row >
                         <Col xs=".5" >
                             <Image style={{ height: "50px", width: "50px", border: "1px solid #E8E9EB" }} src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/45/d10d0f4b-1fca-4b84-bc19-bbf492c040da.png" roundedCircle />
                         </Col>
-                        <Col md="6">
+                        <Col style={{textAlign: "left"}}>
                             <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
                             <p>Delivery</p>
                         </Col>
@@ -66,7 +68,7 @@ function StoreListings() {
                         <Col xs=".5" >
                             <Image style={{ height: "50px", width: "50px", border: "1px solid #E8E9EB" }} src="https://d2d8wwwkmhfcva.cloudfront.net/96x96/d2lnr5mha7bycj.cloudfront.net/warehouse/logo/45/d10d0f4b-1fca-4b84-bc19-bbf492c040da.png" roundedCircle />
                         </Col>
-                        <Col md="6">
+                        <Col style={{textAlign: "left"}}>
                             <h5 style={{ fontWeight: "bold" }}>Kroger</h5>
                             <p>Delivery</p>
                         </Col>
