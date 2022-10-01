5d9bd5c4d0e4f98ad6f4976b7c88d8b13e2ad5ae
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-10-01 00:13:18 -0400

    added StoreListings

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import TopBar from './components/TopBar'
 import Background from './components/Background';
+import StoreListings from './components/StoreListings';
 
 function App() {
   return (
     <div className="App">
       <TopBar/>
       <Background/>
+      <StoreListings/>
     </div>
   );
 }
